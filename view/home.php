cfcee75e0099310a52e4ac72d09bad90fe85cbb0
jevil77 <jerome.blondeau@uha.fr>
2025-02-10 19:04:42 +0100

    Page d'accueil suite

--- view/home.php
+++ view/home.php
@@ -414,25 +414,25 @@
 
 
 <section class="section">
-        <div class="movie-gallery h3">
+        
             <div class="alignement">
            <a href="film.php">
             <h3>Dernières listes de films</h3>
             <a href="dernièresListesFilms.php"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#2c3440"><path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
             </a>
           </div>
-    </div>
+    
     <div class="separator"></div>
   
 
 <div class="alignement-listes">
    <a href="ListesFilms.php">
     <div class="poster">
-    <img src="public\img\companion.jpg" alt="Poster 1">
-    <img src="public\img\themonkey.jpg" alt="Poster 2">
-    <img src="public\img\babygirl.jpg" alt="Poster 3">
-    <img src="public\img\dawnofthedead.jpg" alt="Poster 4">
-    <img src="public\img\mulhollanddrive.jpg" alt="Poster 5">
+        <img src="public\img\companion.jpg" alt="Poster 1">
+        <img src="public\img\themonkey.jpg" alt="Poster 2">
+        <img src="public\img\babygirl.jpg" alt="Poster 3">
+        <img src="public\img\dawnofthedead.jpg" alt="Poster 4">
+        <img src="public\img\mulhollanddrive.jpg" alt="Poster 5">
       <div class="poster-title">
         <h3>Titre de la liste</h3>
         </div>
